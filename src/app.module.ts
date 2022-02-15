1884edda1305d0d1c8209d14702ed66e524b3c67
Fabian Pauli <fabianhenriquepauli@gmail.com>
2022-02-14 22:46:24 -0300

    ajuste acesso db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://fabianhpauli:SUeCJY0MrOFmmmLp@clusterfabian.ip99u.mongodb.net/test') ],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://fabianhpauli:<password>@clusterfabian.ip99u.mongodb.net/test')],
   controllers: [],
   providers: []
 })
 export class AppModule {}
+
